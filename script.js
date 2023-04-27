ce1e0105653d09bca65ec4a2b3acf9a6fc729674
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-04-27 12:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //your code here
+class OutOfRangeError extends Error {
+  constructor(arg) {
+    super(`Expression should only consist of integers and +-/* characters and not ${arg}`);
+    this.name = "OutOfRangeError";
+  }
+}
+
+class InvalidExprError extends Error {
+  constructor() {
+    super("Expression should not have an invalid combination of expression");
+    this.name = "InvalidExprError";
+  }
+}
+
+function evalString(expr) {
+  try {
+    if (/(\+\+|\-\-|\*\*|\/\/|\+\*|\-\*|\*\+|\/\+)/.test(expr)) {
+      throw new InvalidExprError();
+    }
+    if (/^[+/*]/.test(expr)) {
+      throw new SyntaxError("Expression should not start with invalid operator");
+    }
+    if (/[\+\/*-]$/.test(expr)) {
+      throw new SyntaxError("Expression should not end with invalid operator");
+    }
+    if (!/^[\d\s+\-*/]+$/.test(expr)) {
+      throw new OutOfRangeError(expr);
+    }
+    return eval(expr);
+  } catch (err) {
+    if (err instanceof OutOfRangeError || err instanceof InvalidExprError) {
+      console.error(`${err.name}: ${err.message}`);
+    } else {
+      throw err;
+    }
+  }
+}
